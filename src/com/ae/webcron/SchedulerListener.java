e4fc205a87391fc14a45a81b54c087f7335d3f48
adeliz <adelise.skope@gmail.com>
2017-06-16 11:24:02 +0200

    Fix memory leak when Tomcat shutdown

--- src/com/ae/webcron/SchedulerListener.java
+++ src/com/ae/webcron/SchedulerListener.java
@@ -2,9 +2,13 @@ package com.ae.webcron;
 
 import javax.persistence.*;
 import javax.servlet.*;
+import javax.ws.rs.core.Context;
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
  
 public class SchedulerListener implements ServletContextListener {
- 
+	
     // Prepare the EntityManagerFactory & Enhance:
     public void contextInitialized(ServletContextEvent e) {
         com.objectdb.Enhancer.enhance("com.ae.webcron.*");
@@ -20,5 +24,14 @@ public class SchedulerListener implements ServletContextListener {
         EntityManagerFactory emf =
             (EntityManagerFactory)e.getServletContext().getAttribute("emf");
         emf.close();
+        Scheduler scheduler = (Scheduler) e.getServletContext().getAttribute("scheduler");
+        try {
+			scheduler.shutdown( true );// true = wait for jobs to complete
+		     // you may want to give Quartz some extra time to shutdown
+		     //Thread.sleep(1000);
+		} catch (SchedulerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}  
     }
 }
